96db09f364ae3b69df8dee47bb7ca5e7807b4cf5
Aki <aki@a-i-ad.com>
2017-11-08 17:32:06 +0900

    Update Old7

--- src/main/java/com/aiad/learning/java/generics/Old7.java
+++ src/main/java/com/aiad/learning/java/generics/Old7.java
@@ -1,22 +1,12 @@
 package com.aiad.learning.java.generics;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class Old7 {
     public static void main(String[] args) {
-        // 参考までにJava1.4までよくあったやつ 原型使う
+        Long[] longs = new Long[] { 1L, 2L };
+        SomeDto someDto = SomeUtil.generateLongSomeDto2(longs);
 
-        // String しか入らない前提なリスト
-        List lst = new ArrayList();
-        lst.add("val1");
-        lst.add("val2");
+        String[] strs = new String[] { "val1", "val2" };
+        SomeDto someDto2 = SomeUtil.generateStringSomeDto(strs);
 
-        // 拡張 for 文も無かったのだよ
-        for (Iterator itr = lst.iterator(); itr.hasNext();) {
-            String str = (String) itr.next(); // ここでClassCastException出ることがあったみたいな話
-            System.out.println(str);
-        }
     }
 }
